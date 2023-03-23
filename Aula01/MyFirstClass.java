269c6ffd30bf4d02c3466bc71e6a8c4250949dea
Diogo Costa <dmcosta03@ua.pt>
2023-03-23 18:18:00 +0000

    ASd

--- Aula01/MyFirstClass.java
+++ Aula01/MyFirstClass.java
@@ -4,7 +4,7 @@ public class MyFirstClass {
      * @param args
      */
     public static void main(String[] args){
-        System.out.println("Hello VS Code!");
+        System.out.println("Hello VS Code! Eu sou gay");
         int sum = 0;
         for (int i = 1; i <= 100; i++){
             sum += i;
